39da64abe447cc7e17a203fb513d50443445dfb5
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2017-09-20 19:59:44 -0500

    Updated .java file for first lecture

--- 00-example-project/src/intellijTipsAndTricks.java
+++ 00-example-project/src/intellijTipsAndTricks.java
@@ -1,10 +1,3 @@
-/* 
- * Irfaan Khalid
- * Date: 9/15/2017
- *
- * Description: 
- */
-
 public class intellijTipsAndTricks {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
