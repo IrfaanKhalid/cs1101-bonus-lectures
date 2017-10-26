565aeb966adcb92a20e99643612c49797854c8da
Irfaan <irfaan.khalid@vanderbilt.edu>
2017-10-26 13:02:29 -0500

    Skeleton of debugging examples file

---
+++ 00-example-project/src/dealingWithDebugging.java
@@ -0,0 +1,60 @@
+/*
+ * Irfaan Khalid
+ * 10/25/2017
+ *
+ * Description: Debugging examples used to practice Java debugging in CS 1101.
+ */
+
+public class dealingWithDebugging {
+    public static void main(String[] args) {
+        // First, let's figure out where our crashes happen
+        locationDebugging();
+
+        // Next, let's try to play with some String debugging
+        stringDebugging();
+
+        // Now it's time to see some tricky loops
+        loopDebugging();
+
+        // But wait - there's more! Time for some nested loops
+        nestedLoopDebugging();
+
+        // Finally, let's test your true skills with some booleans
+        booleanDebugging();
+    }
+
+    /**
+     * Example for finding the location of a program crash
+     */
+    private static void locationDebugging() {
+        
+    }
+
+    /**
+     * Example for debugging String indexing errors
+     */
+    private static void stringDebugging() {
+
+    } 
+
+    /**
+     * Example for debugging loop errors
+     */
+    private static void loopDebugging() {
+
+    }
+
+    /**
+     * Example for debugging nested loop errors
+     */
+    private static void nestedLoopDebugging() {
+
+    }
+
+    /**
+     * Example for debugging boolean logic errors
+     */
+    private static void booleanDebugging() {
+
+    }
+}
